b9c6f30fab84110e28e1b994421fe7d63d9aad55
Vladul555 <vladigo3@ac.sce.ac.il>
2021-10-28 13:49:00 +0300

    test

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pygame
 from sys import exit
 
 pygame.init()
-WIDTH, HEIGHT = 900, 600
+WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))  ##Opening the window
 pygame.display.set_caption('BattleShips')  ##The name of the window
 clock = pygame.time.Clock()  ## import for time and fps
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Blue')
+test_surface = pygame.image.load('Graphics/water.png')
+
 
 while True:
     for event in pygame.event.get():
@@ -21,5 +21,3 @@ while True:
     pygame.display.update()
     clock.tick(60)  ## limits the fps to 60
 
-if __name__ == "__main__":  ## Calls the main function
-    main()
