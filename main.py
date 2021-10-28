58e198cb44f2dc2410cd3413f7caba7e380f38a6
Vladul555 <vladigo3@ac.sce.ac.il>
2021-10-28 13:25:31 +0300

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from sys import exit
 
 pygame.init()
 WIDTH, HEIGHT = 900, 600
@@ -6,16 +7,19 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))  ##Opening the window
 pygame.display.set_caption('BattleShips')  ##The name of the window
 clock = pygame.time.Clock()  ## import for time and fps
 
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Blue')
 
-def main():
-    Run = True
-    while Run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:  ## Runs until the user exits the window
-                Run = False
-        clock.tick(60)  ## limits the fps to 60
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  ## Runs until the user exits the window
+            pygame.quit()
+            exit()
 
-    pygame.quit()
-testtestestestestest
+    screen.blit(test_surface, (0, 0))
+
+    pygame.display.update()
+    clock.tick(60)  ## limits the fps to 60
+
 if __name__ == "__main__":  ## Calls the main function
     main()
