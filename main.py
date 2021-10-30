4f6dce223af831bfb43effd5421047cc06b4cc50
ImTheRisingPhoenix <sak7520@gmail.com>
2021-10-30 12:12:15 +0300

    Typo

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from sys import exit
 import pygame
 
 # CONSTANTS
-Widht_Main, Widht_grid, Height_Main, Height_Grid = 800, 400, 600, 300
-screen = pygame.display.set_mode((Widht_Main, Height_Main))  # Opening the window
+Width_Main, Width_grid, Height_Main, Height_Grid = 800, 400, 600, 300
+screen = pygame.display.set_mode((Width_Main, Height_Main))  # Opening the window
 pygame.display.set_caption('BattleShips')  # The name of the game
 BLACK = (0, 0, 0)
 ABC = '0ABCDEFGHIG'
@@ -16,7 +16,7 @@ for x in range(11):
 
 def draw_grid():
     blockSize = 30  # Set the size of the grid block
-    for x in range(Widht_Main):
+    for x in range(Width_Main):
         for y in range(Height_Main):
             if 200 < x * blockSize < 520 and 100 < y * blockSize < 430:  # Putting the grid in a normal place
                 rect = pygame.Rect(x * blockSize, y * blockSize, blockSize, blockSize)
