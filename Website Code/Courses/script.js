61d8803671553b7ba91f1eb0b4286bd52a9e52d0
alonle3 <alonle3@ac.sce.ac.il>
2021-11-27 15:03:04 +0200

    Website Update 2.0
    Menu
    Courses
    Questioneer
    (movement between pages)

---
+++ Website Code/Courses/script.js
@@ -0,0 +1,9 @@
+start = document.getElementById("start")
+buttons = document.getElementsByClassName("buttons")
+menu = document.getElementsByClassName("menu")
+
+function pressMenu(buttons.getElementById()) {
+    if (buttons.getElementById() 0){
+        
+    }
+}
