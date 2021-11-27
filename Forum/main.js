019b7812b3bcbdb65e10e33c3f0b8753b24af744
Vladul555 <92775938+Vladul555@users.noreply.github.com>
2021-11-27 17:57:17 +0200

    Update main.js

--- Forum/main.js
+++ Forum/main.js
@@ -1,1 +1,15 @@
-//playsHolder
+
+//Navigation_bar
+
+function hideIconBar(){
+    var IconBar=document.getElementById("iconBar");
+    var navigation=document.getElementById("navigation");
+    IconBar.setAttribute("style","display:none");
+    navigation.classList.remove("hide");
+}
+function showIconBar(){
+    var IconBar=document.getElementById("iconBar");
+    var navigation=document.getElementById("navigation");
+    IconBar.setAttribute("style","display:block");
+    navigation.classList.add("hide");
+}
