6274e7bdf92319ae2427fae95e58e1214324b268
Vladul555 <vladigo3@ac.sce.ac.il>
2021-11-28 21:26:04 +0200

    more stuff

--- Forum/main.js
+++ Forum/main.js
@@ -7,9 +7,17 @@ function hideIconBar(){
     IconBar.setAttribute("style","display:none");
     navigation.classList.remove("hide");
 }
+
 function showIconBar(){
     var IconBar=document.getElementById("iconBar");
     var navigation=document.getElementById("navigation");
     IconBar.setAttribute("style","display:block");
     navigation.classList.add("hide");
+}
+
+//Coment
+
+function showComment(){
+    var commentArea = document.getElementById("comment-area");
+    commentArea.setAttribute("style", "display:none;");
 }
