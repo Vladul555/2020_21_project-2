fde0e170ffb8ae1205d283ef972a1842592e42ec
Vladul555 <vladigo3@ac.sce.ac.il>
2021-11-29 17:52:39 +0200

    Option menu
    No Database yet so only the skeleton

---
+++ Website Code/options/script.js
@@ -0,0 +1,10 @@
+start = document.getElementById("start")
+buttons = document.getElementsByClassName("buttons")
+menu = document.getElementsByClassName("menu")
+
+function pressStart() {
+    if (start == 0){
+        
+        start = 1
+    }
+}
