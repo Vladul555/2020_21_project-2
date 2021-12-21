8db97bdaf35ad001996de22e6465270d8121ab73
ImTheRisingPhoenix <sak7520@gmail.com>
2021-12-21 20:31:33 +0200

    Linked hearts to DB
    linking the hearts to the free user only

--- Website Code/Python/Multiple Questions/script.js
+++ Website Code/Python/Multiple Questions/script.js
@@ -1,5 +1,4 @@
-const quizData = [
-    {
+const quizData = [{
         question: "When was Python Created?",
         a: "1991",
         b: "2005",
@@ -52,10 +51,10 @@ let score = 0
 
 loadQuiz()
 var heart
-if (localStorage.getItem("heart")){
-    heart = localStorage.getItem("heart")
-}
-    else
+if (Number(sessionStorage.getItem("user")) === userTypes["Free"])
+    if (sessionStorage.getItem("heart")) {
+        heart = sessionStorage.getItem("heart")
+    } else
         heart = 3
 
 function loadQuiz() {
@@ -78,7 +77,7 @@ function deselectAnswers() {
 function getSelected() {
     let answer
     answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
+        if (answerEl.checked) {
             answer = answerEl.id
         }
     })
@@ -88,46 +87,52 @@ function getSelected() {
 
 submitBtn.addEventListener('click', () => {
     const answer = getSelected()
-    if(answer) {
-       if(answer === quizData[currentQuiz].correct) {
-           score++
-       }
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++
+        }
 
-       currentQuiz++
+        currentQuiz++
 
-       if(currentQuiz < quizData.length) {
-           loadQuiz()
-       } else {
-           if (score > 2){
+        if (currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            if (score > 2) {
                 quiz.innerHTML = `
                <div  class="quiz-header">
                <h2>You answered ${score}/${quizData.length} questions correctly\nYOU PASSED! 😀</h2>
                <button class="reload" onclick="location.href='/Website Code/Courses/index.html'">Return To Courses</button>
                </div>
                `
-           }
-           else{
-               heart--
-               if (heart > 0) {
-                   localStorage.setItem("heart",heart)
-                   quiz.innerHTML = `
+            } else if (Number(sessionStorage.getItem("user")) === userTypes["Free"]) {
+                heart--
+                if (heart > 0) {
+                    sessionStorage.setItem("heart", heart)
+                    quiz.innerHTML = `
                    <div  class="quiz-header">
                    <h2>You answered ${score}/${quizData.length} questions correctly</h2>
                    <h2>You Haven't Passed 😔,Reamining hearts left ${heart}</h2>
                    <button class="reload" onclick="location.reload()">Reload</button>
                    </div>
                    `
-               }
-               else {
-                   quiz.innerHTML = `
+                } else {
+                    quiz.innerHTML = `
                    <div  class="quiz-header">
                    <h2>You Failed 😢</h2>
                    <button class="reload" onclick="location.href='/Website Code/Courses/index.html'">Return To Courses</button>
                    </div>
                    `
-               }
-           }
-       }
+                }
+            } else {
+                quiz.innerHTML = `
+               <div  class="quiz-header">
+               <h2>You answered ${score}/${quizData.length} questions correctly</h2><br>
+               <h2 style='text-align: center'>You Failed 😢</h2>
+               <button class="reload" onclick="location.href='/Website Code/Courses/index.html'">Return To Courses</button>
+               </div>
+               `
+            }
+        }
     }
 })
 
@@ -171,7 +176,7 @@ run_clock('clockdiv', deadline);
 
 
 var timeout;
-document.onmousemove = function(){
-  clearTimeout(timeout);
-  timeout = setTimeout(function(){alert("We noticed you are AFk\nTaking a break is important!\nWe are awaiting your eager return!");}, 30000);
+document.onmousemove = function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() { alert("We noticed you are AFk\nTaking a break is important!\nWe are awaiting your eager return!"); }, 30000);
 }
