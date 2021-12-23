6df398f5d4ae6ebf6e04b252459b35fc667fea7f
Vladul555 <vladigo3@ac.sce.ac.il>
2021-12-23 10:16:21 +0200

    darkmoe

--- Website Code/Python/Introduction/script.js
+++ Website Code/Python/Introduction/script.js
@@ -65,6 +65,14 @@ loadData()
 /*function loads new data for each page of the theory*/
 function loadData(){
 
+    {
+        if (currentData == 0){
+            document.getElementById('previous').style.visibility = 'hidden';
+        } else {
+            document.getElementById('previous').style.visibility = 'visible';
+        }
+    }
+
     const current_Text_Data = Data[currentData]
     title.innerText = current_Text_Data.title
     mainText.innerText = current_Text_Data.mainText
@@ -106,4 +114,16 @@ var timeout;
 document.onmousemove = function(){
   clearTimeout(timeout);
   timeout = setTimeout(function(){alert("We noticed you are AFk\nTaking a break is important!\nWe are awaiting your eager return!");}, 30000);
+}
+
+if(sessionStorage.getItem('DarkMod')){
+    flag = sessionStorage.getItem('DarkMod')
+    sessionStorage.setItem('DarkMod',flag);
+}
+
+function TestdarkMode() {
+  if (flag == 1){
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+  }
 }
