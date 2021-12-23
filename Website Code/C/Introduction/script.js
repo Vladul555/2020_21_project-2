142664fba492f6487673c83e3d6bc8080fd4afcd
Yushko69 <amit.jakob7@gmail.com>
2021-12-23 19:51:31 +0200

    C programing

--- Website Code/C/Introduction/script.js
+++ Website Code/C/Introduction/script.js
@@ -2,47 +2,47 @@ const Data = [
     {
         /*Page 1 */
         title: "Introduction To C",
-        mainText: "C is a popular programming language. It was created by Guido van Rossum, and released in 1991." ,
-        opt1: "● C is used in Software Development",
-        opt2: "● C is used in Web Development",
-        opt3: "● C is used in Mathematics",
-        opt4: "● C is used in System Scripting",
+        mainText: "C is a popular programming language, developed by Dennis Ritchie at AT&T Bell Labs, the USA, between 1969 and 1973." ,
+        opt1: "● C is used in general-purpose programming",
+        opt2: "● C is a mid-level structured oriented programming language",
+        opt3: "● C is used to develop system application software.",
+        opt4: "● C has been written in assembly language",
     },
     {
         /*Page 2 */
         title: "What can C do?",
-        mainText: "Find out some of the top uses for C, as we explore why it’s such a popular and diverse programming language.",
-        opt1: "● C can connect to database systems. It can also read and modify files",
-        opt2: "● C can be used to handle big data and perform complex mathematics.",
-        opt3: "● C can be used on a server to create web applications and alongside software to create workflows.",
-        opt4: "● C can be used for rapid prototyping, or for production-ready software development.",
+        mainText: "Find out some of the top uses for C, as we explore why it is such a popular programming language.",
+        opt1: "● C language is used for developing system applications.",
+        opt2: "● C language is used to develop databases, It can also read and modify files.",
+        opt3: "● C language is used in developing an operating system",
+        opt4: "● C language is used for compiler production",
     },
     {
         /*Page 3*/
         title: "Why C?",
-        mainText: "The python language is one of the most accessible programming languages available.",
-        opt1: "● C has syntax that allows developers to write programs with fewer lines than some other programming languages.",
-        opt2: "● C has simplified syntax and not complicated, which gives more emphasis on natural language.",
-        opt3: "● C runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.",
-        opt4: "● C has a simple syntax similar to the English language.",
+        mainText: "C language is one of the most accessible programming languages available.",
+        opt1: "● C is the building block for many other programming languages.",
+        opt2: "● Programs written in C are highly portable.",
+        opt3: "● C have Several standard functions that can be used to develop programs.",
+        opt4: "● The modular structure makes code debugging, maintenance, and testing easier.",
     },
     {
         /*Page 4*/
         title:"Indendation",
-        mainText: "C uses indentation to indicate a block of code.indentation in C is very important.",
+        mainText: "C uses indentation of blocks of code to convey program structure. Indendation of code in C is very important.",
         opt1: "● You have to use the same number of spaces in the same block of code, otherwise C will give you an error",
         opt2: "● The number of spaces is up to you as a programmer, but it has to be at least one",
-        opt3: "● C will give you an error if you skip the indentation",
-        opt4: "",
+        opt3: "● every line/operation of code ends with ';'",
+        opt4: "● Indendation is used to clarify the link between control flow constructs such as conditions or loops.",
     },
     {
         /*Page 5*/
         title: "Variables",
         mainText: "Variables are containers for storing data values.",
-        opt1: "● A variable is created the moment you first assign a value to it",
-        opt2: "● Variables do not need to be declared with any particular type, and can even change type after they have been set",
-        opt3: "● You can get the data type of a variable with the type() function",
-        opt4: "",
+        opt1: "● Variables in C are composed by 3 thing. a data type, a name, and a value.",
+        opt2: "● To create a variable in C you must first declare the data type, give the variable a name then assign a value to it.",
+        opt3: "● The name of variables must contain letters, it can also have numbers but it can't only be composed of them.",
+        opt4: "● The value of the variable must match data the type otherwise C will give you an error.",
     }
 
 ];
@@ -92,7 +92,7 @@ nextBtn.addEventListener('click', () => {
            <div class="header">
            <h2 class="test">You completed the theory!</h2>
 
-           <input class="button" type="button" onClick="location.href='/Website Code/C/Python/Multiple Questions/index.html'"
+           <input class="button" type="button" onClick="location.href='/Website Code/C/Multiple Questions/index.html'"
                 value='Start Test!'>
             <button class="button" onclick="location.reload()">Reload</button>
            </div>
