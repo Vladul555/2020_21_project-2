92970ef3852825926e00c9206c55ddf1f19dc5fc
Vladul555 <vladigo3@ac.sce.ac.il>
2021-12-25 16:45:13 +0200

    DOWNLOW button is now working in the python section

--- Website Code/Python/Variables/FinalTest/script.js
+++ Website Code/Python/Variables/FinalTest/script.js
@@ -204,10 +204,12 @@ function TestdarkMode() {
 if (Number(sessionStorage.getItem("user")) === userTypes["Free"]){
     document.getElementById('status__logo').src = "./images/FREE.png";
     //document.getElementById('Copy').style.visibility = 'hidden';
+    //document.getElementById('Download').style.visibility = 'hidden';
 }
 else{
     document.getElementById('status__logo').src = "./images/PRO.png";
     document.getElementById('Copy').style.visibility = 'visible';
+    document.getElementById('Download').style.visibility = 'visible';
 }
 
 function Copy_text() {
@@ -221,4 +223,25 @@ function Copy_text() {
     document.execCommand('copy');
     document.body.removeChild(el);
     alert("the question was copied to the clipboard!");
+}
+
+function Download_file() {
+    let downloadText = quizData[currentQuiz].question + ' ' + quizData[currentQuiz].a + '. ' + quizData[currentQuiz].b + '. ' + quizData[currentQuiz].c + '. ' + quizData[currentQuiz].d + '. ' ;
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([downloadText], { type: 'text/plain' });
+    const sFileName = 'formData.txt';	   // The file to save the data.
+
+    let newLink = document.createElement("a");
+    newLink.download = sFileName;
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click(); 
 }
