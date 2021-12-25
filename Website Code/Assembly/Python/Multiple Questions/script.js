b6e00119a29f69cb2adf14505df3a1cb258318be
Vladul555 <vladigo3@ac.sce.ac.il>
2021-12-25 15:43:17 +0200

    download option for the pro user

--- Website Code/Assembly/Python/Multiple Questions/script.js
+++ Website Code/Assembly/Python/Multiple Questions/script.js
@@ -203,3 +203,24 @@ function Copy_text() {
     document.body.removeChild(el);
     alert("the question was copied to the clipboard!");
 }
+
+function Download_file() {
+    var downloadText = quizData[currentQuiz].question + ' ' + quizData[currentQuiz].a + '. ' + quizData[currentQuiz].b + '. ' + quizData[currentQuiz].c + '. ' + quizData[currentQuiz].d + '. ' ;
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([downloadText], { type: 'text/plain' });
+    const sFileName = 'formData.txt';	   // The file to save the data.
+
+    let newLink = document.createElement("a");
+    newLink.download = sFileName;
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click(); 
+}
